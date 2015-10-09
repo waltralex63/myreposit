bb7db19043d0bbc4df7c15c2673e3b93f69e9372
walter <waltralex63@gmail.com>
2015-10-09 09:38:50 -0600

    agregando css

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Postear
+from django.utils import timezone
+
+def post_list(request):
+    posts = Postear.objects.filter(fecha_publicada__lte=timezone.now()).order_by('fecha_publicada')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
 
 # Create your views here.
