7d81b0ed64460e257526d46f6e2e3363e6fd2091
walter <waltralex63@gmail.com>
2015-10-13 10:16:47 -0600

    Se creo un nuevo archivo y modifique

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from django.shortcuts import render
 from .models import Postear
 from django.utils import timezone
@@ -6,5 +7,7 @@ def post_list(request):
     posts = Postear.objects.filter(fecha_publicada__lte=timezone.now()).order_by('fecha_publicada')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
-
+def post_detail(request, pk):
+     post = get_object_or_404(Post, pk=pk)
+     return render(request, 'blog/post_detail.html', {'post': post})
 # Create your views here.
